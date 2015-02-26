53e4ffe97a0c3eb3d43604dd940f4856cc1dae3b
gyeprefos <farkas48@uniba.sk>
2015-02-26 08:53:30 +0100

    final version

--- ckanext/dataset_rating/plugin.py
+++ ckanext/dataset_rating/plugin.py
@@ -6,11 +6,14 @@ import ckan.logic
 import ckan.model as model
 from ckan.common import _, c
 import logging
+import rating
+
 
 
 class DatasetRatingPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.ITemplateHelpers, inherit=False)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
@@ -19,3 +22,6 @@ class DatasetRatingPlugin(plugins.SingletonPlugin):
         map.connect('rate_dataset', '/dataset/rate/', action='RateDataset', controller="ckanext.dataset_rating.rating:RatingController")
         
         return map
+    def get_helpers(self):
+    	return {'avg': rating.avg,
+    			'can_rate': rating.can_rate}
--- ckanext/dataset_rating/rating.py
+++ ckanext/dataset_rating/rating.py
@@ -12,6 +12,7 @@ import ckan.plugins as p
 from ckan.common import _, c, g
 import ckan.plugins.toolkit as toolkit
 
+import db
 
 import uuid
 import logging
@@ -25,10 +26,84 @@ TODO:
 -current rating (helper)
 
 '''
+def create_dataset_rating_table(context):
+    if db.dataset_rating_table is None:
+        db.init_db(context['model'])
+
+@ckan.logic.side_effect_free
+def in_rating_db(context, data_dict):
+    create_dataset_rating_table(context)
+    if data_dict['user_id'] == None or data_dict['user_id'] == '':
+        return False
+    res = db.DatasetRating.get(**data_dict)
+    if res:
+        for i in res:
+            if i.user_id == data_dict['user_id']:
+                return True
+
+    return False
+
+def can_rate(user_id):
+    context = {'model': model, 'session': model.Session,
+                   'user': c.user or c.author, 'auth_user_obj': c.userobj,
+                   'for_view': True}
+    try:
+        logic.check_access('tags_admin', context)
+        return True
+    except logic.NotAuthorized:
+        return False
+    return False
+def avg(dataset_id):
+    context = {'model': model, 'session': model.Session,
+                   'user': c.user or c.author, 'auth_user_obj': c.userobj,
+                   'for_view': True}
+    data_dict = {'dataset_id': dataset_id}
+    create_dataset_rating_table(context)
+
+    result = db.DatasetRating.get(**data_dict)
+    
+    if len(result) != 0:
+        sum=0.0
+        for i in result:
+            sum+=float(i.rating)
+
+        return sum/len(result)
+    else:
+        return 0;
+
+@ckan.logic.side_effect_free
+def new_rating(context, data_dict):
+    create_dataset_rating_table(context)
+    data_dict2 = {'dataset_id':data_dict['dataset_id'], 'user_id':data_dict['user_id']}
+    if in_rating_db(context, data_dict2):
+        res = db.DatasetRating.get(**data_dict2)[0]
+        res.rating = data_dict['rating']
+        res.save()
+        session = context['session']
+        session.commit()
+    else:
+        info = db.DatasetRating()
+        info.id = unicode(uuid.uuid4()) 
+        info.user_id = data_dict['user_id']
+        info.dataset_id = data_dict['dataset_id']
+        info.rating = data_dict['rating']
+        info.save()
+        session = context['session']
+        session.add(info)
+        session.commit()
+    return {"status":"success"}
+
+
 class RatingController(base.BaseController):
     def RateDataset(self):
+    	context = {'model': model, 'session': model.Session,
+                   'user': c.user or c.author, 'auth_user_obj': c.userobj,
+                   'for_view': True}
         rating = logic.clean_dict(df.unflatten(logic.tuplize_dict(logic.parse_params(base.request.params))))
-        logging.warning('MAJOM')
         logging.warning(rating)
-        related_id = rating['dataset_id']
-        return h.redirect_to(controller='package', action='read', id=related_id)
+        dataset_id = rating['dataset_id']
+        rating = rating['value']
+        data_dict = {'rating': rating, 'dataset_id':dataset_id, 'user_id':c.userobj.id}
+        new_rating(context, data_dict)
+
+        return h.redirect_to(controller='package', action='read', id=dataset_id)
